bb636decbe9d7d140710a3c405e0d850d13b21eb
young <2038589669@qq.com>
2019-10-08 20:19:02 +0800

    2019-10-08

--- 1.js
+++ 1.js
@@ -1,6 +1,10 @@
 (function (){
     function tansform(str){
         var re=/-(\w)/g;
+        /**
+         * replace正则表达式
+         * item,olditem代表正则表达式中的括号里面匹配到的值,从item开始
+         */
         return str.replace(re,(item,olditem)=>{
             return olditem.toUpperCase();
         })
