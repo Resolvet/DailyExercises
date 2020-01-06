ae1f3ab996cfd626aef7ec35b1a373d8e85aa982
young <2038589669@qq.com>
2020-01-07 00:01:45 +0800

    2

--- start/src/App.vue
+++ start/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-		{{num}}
+    <!-- <img src="./assets/logo.png"> -->
+		<!-- {{num}} -->
 		<!-- <button @click="incer">点击同步添加</button> -->
-		<button @click="asyn">异步添加</button>
+		<!-- <button @click="asyn">异步添加</button> -->
 		
-		<div class=" ql-editor">
+		<!-- <div class=" ql-editor">
 				<HelloWorld></HelloWorld>
-		</div>
+		</div> -->
 		
 		<!--  -->
 		<!-- <el-form-item label="图标">
@@ -21,6 +21,47 @@
 		    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
 		  </el-upload>
 		</el-form-item> -->
+		<div class="bought">
+			<div class="bought_title">
+				<ul>
+					<li>宝贝</li>
+					<li>实付款</li>
+					<li>交易操作</li>
+				</ul>
+			</div>
+			<div class="bought_content">
+			<div class="bought-table-mod">
+				<div>
+					<b>2019-12-30</b>
+					<span>订单号: 788082659730998363</span>
+				</div>	
+				<b>删除</b>
+			</div>
+			<div class="bought_body">
+				<div class="bought_img">
+					<img src="https://img.alicdn.com/imgextra/i4/2981688681/O1CN01pCxCAg2DzwoahqAOd_!!2981688681.jpg_80x80.jpg" alt="">
+					<p>
+						秋季长袖T恤秋衣男士纯棉打底衫体恤内搭白色男装上衣服纯色潮流
+					</p>
+					<span>
+						￥17.90
+					</span>
+					<span>x1</span>
+				</div>
+				<div class="bought_pice">
+					<span>￥17.90</span>
+				</div>
+				<div class="bought_pice">
+					<span style="border-right: 1px solid #ddd;">
+					<p>
+						交易关闭
+					    订单详情
+					</p>
+					</span>
+				</div>
+			</div>
+			</div>
+		</div>
   </div>
 </template>
 
@@ -54,17 +95,102 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-#app{
-	width: 1200px;
-	margin: 0 auto;
-}
+<style lang="less">
+	*{
+		margin: 0;
+		padding: 0;
+	}
+	.bought{
+		width: 1190px;
+		margin: 0 auto;
+		.bought_title{
+			background: #eee;
+			height: 40px;
+			margin-bottom: 20px;
+			ul {
+				width: 100%;
+				height: 39px;
+				line-height: 39px;
+				> li{
+					float: left;
+					list-style: none;
+					text-align: center;
+					&:nth-child(1){
+						width: 500px;
+					}
+					&:nth-child(2){
+						width: 180px;
+					}
+					&:nth-child(3){
+						width: 430px;
+						text-align: right;
+					}
+				}
+			}
+		}
+		.bought_content{
+			border: 1px solid #ddd;
+		.bought-table-mod{
+			width: 100%;
+			display: flex;
+			height: 50px;
+			align-items: center;
+			justify-content: space-between;
+			background: #eee;
+			padding: 0 15px;
+			box-sizing: border-box;
+			>div{
+				b{
+					margin-right: 25px;
+				}
+			}
+			> b{
+				display: block;
+				width: 200px;
+				text-align: center;
+			}
+		}
+		.bought_body{
+			width: 100%;
+			height: 120px;
+			padding: 0 15px;
+			box-sizing: border-box;
+			align-items: center;
+			display: flex;
+			.bought_img{
+				display: flex;
+				height: 120px;
+				align-items: center;
+				>img{
+					width: 80px;
+					height: 80px;
+					margin-right: 5px;
+				}
+				p{
+					width: 220px;
+					margin-right: 30px;
+					font-size: 14px;
+					color: #333;
+				}
+				span{
+					margin: 0 25px;
+					color: #444;
+					font-size: 14px;
+				}
+			}
+			.bought_pice{
+				> span{
+					display: block;
+					width: 180px;
+					height: 120px;
+					border-left: 1px solid #eee;
+					line-height: 120px;
+					text-align: center;
+					color: #444;
+				    font-size: 14px;
+				}
+			}
+		}
+		}
+	}
 </style>
--- start/src/router/index.js
+++ start/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Home'
+import Page from '@/components/Page'
 
 Vue.use(Router)
 export default new Router({
@@ -9,6 +10,10 @@ export default new Router({
       path: '/actions',
       name: 'actions',
       component: Index,
-    }
+    },
+	  {
+			path: 'page',
+			components: Page
+		}
   ]
 })
